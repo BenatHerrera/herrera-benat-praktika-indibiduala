a0f845de62ab20c3ba83cd6647726b9cf83b663d
TitoB8 <bherreras23@fpsanjorge.com>
2025-02-14 23:01:40 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Herrera_Beñat_Praktika_Indibiduala',
+  webDir: 'www'
+};
+
+export default config;
